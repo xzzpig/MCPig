1e8ebdcb035d6e45d94d1e1a95daa73b1c5741bd
xzzpig <1146628972@qq.com>
2016-05-21 12:06:34 +0800

    up

--- src/com/github/xzzpig/mcpig/Debuger.java
+++ src/com/github/xzzpig/mcpig/Debuger.java
@@ -1,18 +1,16 @@
-
 package com.github.xzzpig.mcpig;
 
 import org.bukkit.entity.Player;
 
 import com.github.xzzpig.pigapi.bukkit.TPlayer;
 
-
 public class Debuger {
 	public static long time;
-	
+
 	private static boolean debug = true;
-	
+
 	public static void print(Object s) {
-		if (debug== false)
+		if (debug == false)
 			return;
 		System.out.println("\n****************\n" + s + "\n****************");
 		for (Player p : TPlayer.getAllPlayers()) {
--- src/com/github/xzzpig/mcpig/listener/TCPListener.java
+++ src/com/github/xzzpig/mcpig/listener/TCPListener.java
@@ -1,13 +1,26 @@
 package com.github.xzzpig.mcpig.listener;
 
 import com.github.xzzpig.mcpig.Debuger;
+import com.github.xzzpig.pigapi.TData;
+import com.github.xzzpig.pigapi.bukkit.MD5;
+import com.github.xzzpig.pigapi.customevent.ClientConnectEvent;
 import com.github.xzzpig.pigapi.customevent.ServerDataReachEvent;
 import com.github.xzzpig.pigapi.event.EventHandler;
 import com.github.xzzpig.pigapi.event.Listener;
 
 public class TCPListener implements Listener {
 	@EventHandler
-	public void onServerDataReach(ServerDataReachEvent event){
-		Debuger.print(event.getClient().s.getInetAddress().getHostName()+":\n"+event.getData());
+	public void onServerDataReach(ServerDataReachEvent event) {
+		Debuger.print(event.getClient().s.getInetAddress().getHostName()
+				+ ":\n" + event.getData());
+	}
+
+	@EventHandler
+	public void onClientLink(ClientConnectEvent event) {
+		event.getClient().data.setString("key",
+				MD5.GetMD5Code(event.getClient().toString()));
+		event.getClient().sendData(
+				new TData().setString("action", "key").setString("key",
+						MD5.GetMD5Code(event.getClient().toString())));
 	}
 }
